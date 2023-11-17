dc5f43d76574f0981feb72fcb0bb00636c775d31
KeynerBarrios <barrioskeyner693@gmail.com>
2023-11-16 21:05:00 -0500

    vistas de order y pago

--- src/presentation/pages/order/Order.tsx
+++ src/presentation/pages/order/Order.tsx
@@ -1,3 +1,4 @@
+import './Styles.css'
 import { useState } from "react";
 
 const Order = () => {
@@ -5,21 +6,87 @@ const Order = () => {
 
   const view1 = (
     <>
-      <h1>Vista 1</h1>
-      <button onClick={()=>setCurrentView("PayOrder")}>Pagar</button>
+      <div className="order-contend">
+        <img className='order-contend-img' src="/confetti.svg" alt="" />
+        <h1 className='order-contend-title'>Esperando por pago</h1>
+        <img className='order-contend-img1' src="/Asset 1.svg" alt="" />
+        <div className='order-contend-state'>
+          <img src="/Loading bar.svg" alt="" />
+          <p className='order-state'>Estado del pedido</p>
+        </div>
+        <button className='order-contend-pay' onClick={()=>setCurrentView("PayOrder")}>Pagar</button>
+      </div>
+      
     </>
   );
 
   const view2 = (
     <>
-      <h1>Vista 2</h1>
-      <button onClick={()=>setCurrentView("OrderPaid")}>Next</button>
+    <div className='pay-contend'>
+        <div className='pay-contend-bar'>Pagos</div>
+
+        <p className='pay-title'>Por favor revisa los detalles del pedido</p>
+
+        <div className='pay-contend-info'>
+            <div className='pay-contend-order'>
+                <img src="/Group.svg" alt="" />
+                <p>Pedido</p>
+            </div>
+            <div className='container-pay'>
+                <p className='pay-contend-p'>Entidad de pago</p>
+                <p className='pay-contend-p1'>Nequi</p>
+            </div>
+            <div className='container-pay'>
+                <p className='pay-contend-p'>Tipo de factura</p>
+                <p className='pay-contend-p1'>Orden de comida</p>
+            </div>
+            <div className='container-pay'>
+                <p className='pay-contend-p'>numero de factura</p>
+                <p className='pay-contend-p1'>NN00111345</p>
+            </div>
+            <div className='container-pay'>
+                <p className='pay-contend-p'>Id de transacción</p>
+                <p className='pay-contend-p1'>EUE0111345</p>
+            </div>
+            <div className='container-pay'>
+                <p className='pay-contend-p'>Pedido</p>
+                <p className='pay-contend-p1'>Ensalada de salmon</p>
+                <p className='pay-contend-p1'>Macaroni de pasta</p>
+                <p className='pay-contend-p1'>Fried ice</p>
+            </div>
+            <div className='container-pay'>
+                <p className='pay-contend-p'>Monto a pagar</p>
+                <p className='pay-contend-p1'>N20,000.00</p>
+            </div>
+        </div>
+        
+        <button className='pay-contend-conf' onClick={()=>setCurrentView("OrderPaid")}>Confirmar</button>
+        
+    </div>
+      
     </>
   );
 
   const view3 = (
     <>
-      <h1>Vista 3</h1>
+    
+    <div className='order-contend'>
+      <div className='order-contend-img'>
+        <img  src="/confetti.svg" alt="" />
+      </div>
+
+      <h1 className='order-contend-title'>Pago realizado</h1>
+
+      <div className='order-contend-img1'>
+        <img src="/Asset 5.svg" alt="" />
+      </div>
+
+      <div className='order-contend-state'>
+        <img src="/Loading bar.svg" alt="" />
+        <p>Estado del pedido</p>
+      </div>
+      
+    </div>
      
     </>
   );
